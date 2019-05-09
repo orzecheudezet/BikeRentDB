cce4e8db37ddc3b2ee0358907ead8baca05a30e2
orzecheudezet <krzysztof.m.orzechowski@gmail.com>
2019-05-09 19:16:55 +0200

    koniec zajec

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from Bike import *
 from BikeRepository import *
+from functions import *
 import pyodbc
 
 
 print("Welcome to Bike Rent.")
-userChar = ''
+userChoice = ''
 
-while userChar != 'q':
+while userChoice != 'q':
     print("1. Add bike")
-    userChar = input("Enter num or q to quit: ")
+    print("2. Add user")
+    userChoice = input("Enter num or q to quit: ")
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=USER-KOMPUTER\SQLEXPRESS;DATABASE=BikeRents;Trusted_Connection=yes')
     cursor = cnxn.cursor()
 
-    if userChar == '1':
-        bike = Bike(None, None, None, None)
-        bike.AddBike()
+    if userChoice == '1':
+        bike = createBikeFromInput()
         bikeRep = BikeRepository(cursor)
         bikeRep.save(bike)
         cnxn.commit()
+
+    #if userChoice == '2':
+
+
+
 
 
