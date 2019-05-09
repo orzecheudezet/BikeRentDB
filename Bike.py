295da9ee3e9b302fb121742af792b736a7506969
orzecheudezet <krzysztof.m.orzechowski@gmail.com>
2019-05-09 18:14:08 +0200

    1. Add bike

--- Bike.py
+++ Bike.py
@@ -4,3 +4,12 @@ class Bike:
         self.color = color
         self.size = size
         self.brand = brand
+
+    def __str__(self):
+        return ("Object: " + str(self.id) + " " + str(self.color)+ " " + str(self.size) + " " + str(self.brand))
+
+    def AddBike(self):
+        self.id = None
+        self.color = input("Enter color: ")
+        self.size = input("Enter size: ")
+        self.brand = input("Enter brand: ")
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("DUPA BLADA")
+from Bike import *
+from BikeRepository import *
+import pyodbc
+
+
+print("Welcome to Bike Rent.")
+userChar = ''
+
+while userChar != 'q':
+    print("1. Add bike")
+    userChar = input("Enter num or q to quit: ")
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=USER-KOMPUTER\SQLEXPRESS;DATABASE=BikeRents;Trusted_Connection=yes')
+    cursor = cnxn.cursor()
+
+    if userChar == '1':
+        bike = Bike(None, None, None, None)
+        bike.AddBike()
+        bikeRep = BikeRepository(cursor)
+        bikeRep.save(bike)
+        cnxn.commit()
+
+
